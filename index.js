9f8880f6b6797ded76a1765c15884a72dc6d604e
Diogo CS Berner <diogoberner@gmail.com>
2025-01-20 21:52:16 -0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const inputItem = document.getElementById("input-item")
 
-console.log(inputItem.value);
+const addButton = document.getElementById("adicionar-item")
+let counter = 0
+
+addButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (inputItem.value === " "){
+        alert("Por favor, insira um item!")
+        return
+    }
+
+    const listItem = document.createElement("li")
+    const listItemContainer = document.createElement("div")
+    listItemContainer.classList.add("lista-item-container")
+    const inputCheckbox = document.createElement("input")
+    inputCheckbox.type = "checkbox"
+    inputCheckbox.id = "checkbox-" + counter++
+    const itemName = document.createElement("p")
+    itemName.innerText = inputItem.value
+
+    listItemContainer.appendChild(inputCheckbox)
+    listItemContainer.appendChild(itemName)
+})
