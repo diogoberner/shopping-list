299b874705930c2ab7732e0f815d6c07b9b7b9d8
Diogo CS Berner <diogoberner@gmail.com>
2025-01-22 07:10:32 -0300

    module

--- index.js
+++ index.js
@@ -1,52 +1,13 @@
-const inputItem = document.getElementById("input-item")
+import { createItemList } from "./scripts/createItemList.js"
 
 const shoppingList = document.getElementById("lista-de-compras")
 
 const addButton = document.getElementById("adicionar-item")
-let counter = 0
+
 
 addButton.addEventListener("click", (e) => {
-    e.preventDefault()
-    if (inputItem.value === ""){
-        alert("Por favor, insira um item!")
-        return
-    }
-
-    const listItem = document.createElement("li")
-    const listItemContainer = document.createElement("div")
-    listItemContainer.classList.add("lista-item-container")
-    const inputCheckbox = document.createElement("input")
-    inputCheckbox.type = "checkbox"
-    inputCheckbox.id = "checkbox-" + counter++
-    const itemName = document.createElement("p")
-    itemName.innerText = inputItem.value
-
-    inputCheckbox.addEventListener("click", () => {
-        if (inputCheckbox.checked) {
-            itemName.style.textDecoration = "line-through"
-        } else {
-            itemName.style.textDecoration = "none"
-        }
-    })
-
-    const weekday = new Date().toLocaleDateString("pt-BR", {
-        weekday: "long"
-    })
-    const date = new Date().toLocaleDateString("pt-BR")
-    const hour = new Date().toLocaleString("pt-BR", { hour: "2-digit", minute: "2-digit" })
-    const completeDate = `${weekday} (${date}) às ${hour}`
-    
-    const dataText = document.createElement("p")
-    dataText.innerText = completeDate
-    dataText.classList.add("texto-data")
-
-    listItemContainer.appendChild(inputCheckbox)
-    listItemContainer.appendChild(itemName)
-
-    listItem.appendChild(listItemContainer)
-    listItem.appendChild(dataText)
+    const listItem = createItemList(e)
     shoppingList.appendChild(listItem)
-
     checkEmptyList()
 })
 
@@ -54,7 +15,6 @@ const emptyMessage = document.querySelector(".mensagem-lista-vazia")
 
 function checkEmptyList () {
     const listItem = shoppingList.querySelectorAll(".lista-item-container")
-    console.log(listItem)
 
     if(listItem.length != 0){
         emptyMessage.style.display = "none"
