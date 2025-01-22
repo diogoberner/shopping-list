0550b4b9f2043d64c0958eb830aa1bced2e51737
Diogo CS Berner <diogoberner@gmail.com>
2025-01-22 05:08:25 -0300

    date added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const inputItem = document.getElementById("input-item")
 
+const shoppingList = document.getElementById("lista-de-compras")
+
 const addButton = document.getElementById("adicionar-item")
 let counter = 0
 
 addButton.addEventListener("click", (e) => {
     e.preventDefault()
-    if (inputItem.value === " "){
+    if (inputItem.value === ""){
         alert("Por favor, insira um item!")
         return
     }
@@ -19,6 +21,29 @@ addButton.addEventListener("click", (e) => {
     const itemName = document.createElement("p")
     itemName.innerText = inputItem.value
 
+    inputCheckbox.addEventListener("click", () => {
+        if (inputCheckbox.checked) {
+            itemName.style.textDecoration = "line-through"
+        } else {
+            itemName.style.textDecoration = "none"
+        }
+    })
+
+    const weekday = new Date().toLocaleDateString("pt-BR", {
+        weekday: "long"
+    })
+    const date = new Date().toLocaleDateString("pt-BR")
+    const hour = new Date().toLocaleString("pt-BR", { hour: "2-digit", minute: "2-digit" })
+    const completeDate = `${weekday} (${date}) às ${hour}`
+    
+    const dataText = document.createElement("p")
+    dataText.innerText = completeDate
+    dataText.classList.add("texto-data")
+
     listItemContainer.appendChild(inputCheckbox)
     listItemContainer.appendChild(itemName)
+
+    listItem.appendChild(listItemContainer)
+    listItem.appendChild(dataText)
+    shoppingList.appendChild(listItem)
 })
